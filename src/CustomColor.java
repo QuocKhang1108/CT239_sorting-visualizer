a90c4ea0b6e827bf6e53edd148010b60bf5c2a2d
unknown <phamquockhang1108@gmail.com>
2024-03-29 01:00:12 +0700

    change colors

--- src/CustomColor.java
+++ src/CustomColor.java
@@ -1,18 +1,18 @@
 import java.awt.*;
 
 public class CustomColor {
-    public static Color mainBackground = new Color(30, 31, 34);
-    public static Color canvasBackground = new Color(43, 45, 48);
-    public static Color btnEntered = new Color(27, 55, 134, 255);
-    public static Color btnPressed = new Color(0, 154, 139, 255);
+    public static Color mainBackground = new Color(1, 4, 9);
+    public static Color canvasBackground = new Color(13, 17, 23);
+    public static Color btnEntered = new Color(31, 110, 234, 255);
+    public static Color btnPressed = new Color(35, 133, 54, 255);
     public static Color originalColor = new Color(171, 214, 228);
     public static Color comparingColor = new Color(253, 164, 0);
     public static Color swappingColor = new Color(0, 127, 0);
-    public static Color sortedColor = new Color(15, 78, 155);
-    public static Color scanColor = new Color(0, 154, 139);
-    public static Color text = new Color(39, 102, 208);
-    public static Color textSelected = new Color(223, 223, 223);
-    public static Color textInput = new Color(28, 27, 27);
-    public static Color boxInput = new Color(215, 209, 209);
+    public static Color sortedColor = new Color(31, 110, 234);
+    public static Color scanColor = new Color(3, 94, 232);
+    public static Color text = new Color(0, 61, 176);
+    public static Color textSelected = new Color(229, 235, 241);
+    public static Color textInput = new Color(229, 235, 241);
+    public static Color boxInput = new Color(13, 17, 23);
     public static Color red = new Color(255, 0, 0);
 }
