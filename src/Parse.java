3c2f6b96eca6766536ffb38ea1ec562e848e31c1
QuocKhang <phamquockhang1108@gmail.com>
2024-04-02 00:10:29 +0700

    add necessary functions

--- src/Parse.java
+++ src/Parse.java
@@ -1,2 +1,9 @@
 public class Parse {
+    public int parseHeight(int value) {
+        return value * 6 + 6;
+    }
+
+    public int parseValue(int height) {
+        return (height - 6) / 6;
+    }
 }
