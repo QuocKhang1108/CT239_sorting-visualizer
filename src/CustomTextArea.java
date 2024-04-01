0bd0c482146ae2a51662456bc8e6cfee4f2304aa
QuocKhang <phamquockhang1108@gmail.com>
2024-04-01 20:51:57 +0700

    rename file

--- src/CustomTextArea.java
+++ src/CustomTextArea.java
@@ -1,2 +1,15 @@
-public class CustomTextArea {
+import javax.swing.*;
+import java.awt.*;
+
+public class CustomTextArea extends JTextArea {
+    public CustomTextArea() {
+        super();
+        setLineWrap(true);
+        setWrapStyleWord(true);
+        setFont(new Font(null, Font.ITALIC, 15));
+        setForeground(CustomColor.text);
+        setBackground(CustomColor.mainBackground);
+    }
+
+
 }
